ca3c90e4793596075d1b6dbf03257cfa670ed121
CainFernandez <65930136+CainFernandez@users.noreply.github.com>
2022-11-06 11:34:26 -0500

    Creación de la entidad Categorias

---
+++ MajaTours/Data/Entities/Category.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MajaTours.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        
+        [Display(Name = "Categoria")]
+        [MaxLength(50, ErrorMessage = "El Campo {0} debe tener Máximo {1} caractéres.")]
+        [Required (ErrorMessage = "El campo {0} es obligatorio.")]
+        public string? Name { get; set; }
+    }
+}
