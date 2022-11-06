f650176d0c22eb8f396ad3e9eb19b03b8e47c637
CainFernandez <65930136+CainFernandez@users.noreply.github.com>
2022-11-06 11:29:17 -0500

    Conexión  a la BD y sus configuraciones

--- MajaTours/Program.cs
+++ MajaTours/Program.cs
@@ -1,7 +1,16 @@
+using MajaTours.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Configuracion para conectar a la base de datos
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
