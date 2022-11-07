9af1cc0a12d5b69d52d7c514127596de8a84c222
CainFernandez <65930136+CainFernandez@users.noreply.github.com>
2022-11-06 19:51:10 -0500

    Creación del Modelo vista para editar un usuario.

---
+++ MajaTours/Models/EditUserViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MajaTours.Models
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        [Display(Name = "Nombres")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Teléfono")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string PhoneNumber { get; set; }
+    }
+}
