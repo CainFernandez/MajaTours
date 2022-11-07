799e2df02d3c188bd9a50bedbe8396eabe2d4670
CainFernandez <65930136+CainFernandez@users.noreply.github.com>
2022-11-06 19:54:32 -0500

    Agregación del controlador Register
    - Agregación de la inyección a la BD.

--- MajaTours/Controllers/AccountController.cs
+++ MajaTours/Controllers/AccountController.cs
@@ -1,53 +1,96 @@
+using MajaTours.Data;
+using MajaTours.Data.Entities;
+using MajaTours.Enums;
 using MajaTours.Helpers;
 using MajaTours.Models;
 using Microsoft.AspNetCore.Mvc;
-
 namespace MajaTours.Controllers
 {
-   public class AccountController : Controller
+    public class AccountController : Controller
     {
+        private readonly DataContext _context;
         private readonly IUserHelper _userHelper;
-
-        public AccountController(IUserHelper userHelper)
+        public AccountController(DataContext context, IUserHelper userHelper)
         {
+            _context = context;
             _userHelper = userHelper;
         }
-
         public IActionResult Login()
         {
             if (User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Index", "Home");
             }
-
             return View(new LoginViewModel());
         }
 
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
+                Microsoft.AspNetCore.Identity.SignInResult result = await _userHelper.LoginAsync(model);
+                if (result.Succeeded)
                 {
-                    Microsoft.AspNetCore.Identity.SignInResult result = await _userHelper.LoginAsync(model);
-                    if (result.Succeeded)
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                    ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
+                    return RedirectToAction("Index", "Home");
                 }
-                return View(model);
+                ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos.");
             }
+            return View(model);
+        }
         public async Task<IActionResult> Logout()
         {
             await _userHelper.LogoutAsync();
             return RedirectToAction("Index", "Home");
         }
 
-         //------- PAGINA NO AUTORIZADAD --------
+        //----------- RGISTRA NUEVOS USUARIOS ----------
+            public async Task<IActionResult> Register()
+            {
+                AddUserViewModel model = new AddUserViewModel
+                {
+                    Id = Guid.Empty.ToString(),
+                    UserType = UserType.User,
+                };
+                return View(model);
+            }
+
+            //--METODO POST.
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Register(AddUserViewModel model)
+            {
+                if (ModelState.IsValid) 
+                {
+                     
+                    User user = await _userHelper.AddUserAsync(model);
+                    if (user == null)
+                    {
+                        ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado.");
+                        return View(model);
+                    }
+                    LoginViewModel loginViewModel = new LoginViewModel
+                    {
+                        Password = model.Password,
+                        RememberMe = false,
+                        Username = model.Username
+                    };
+
+                    var result2 = await _userHelper.LoginAsync(loginViewModel);
+                    if (result2.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                return View(model);
+            }
+        // --- FIN  ----- 
+
+
+        //------- PAGINA NO AUTORIZADAD --------
         public IActionResult NotAuthorized()
         {
             return View();
         }
     }
-
 }
